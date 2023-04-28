141b77d0ba149709c00d18f1b9f051bb27331740
Amal Sigi <amalsiji4764@gmail.com>
2023-04-28 09:07:57 +0530

    hover update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { FeedComponent } from './modules/feed/feed/feed.component';
 import { FriendsComponent } from './modules/friends/friends/friends.component';
 import { NavComponent } from './modules/nav/nav/nav.component';
+import { LoginComponent } from './modules/auth/login/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, FriendsComponent, FeedComponent],
+  declarations: [AppComponent, NavComponent, FriendsComponent, FeedComponent, LoginComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
